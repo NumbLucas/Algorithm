4830ea3a2d6e4b5a725049f8afa815d30d6b8633
NumbLucas <794191551@qq.com>
2019-09-03 09:55:00 +0800

    tsp

--- src/com/numbguy/ByteDance/tsp.java
+++ src/com/numbguy/ByteDance/tsp.java
@@ -1,9 +1,6 @@
 package com.numbguy.ByteDance;
 
 import java.util.Scanner;
-
-import sun.swing.BakedArrayList;
-
 /*
 链接：https://www.nowcoder.com/questionTerminal/3d1adf0f16474c90b27a9954b71d125d
 来源：牛客网
@@ -28,50 +25,40 @@ import sun.swing.BakedArrayList;
 输出
 13
 */
-
 public class tsp {
-    private static int min_dis;
-    //public static void main(String[] args) {
-
-        // Scanner sc = new Scanner(System.in);
-        // int cityNum = sc.nextInt();
-        // int[][] dis = new int[cityNum][cityNum];
-        // boolean[] visited = new boolean[cityNum];
-        // int min_length = Integer.MAX_VALUE;
-        // int current_length = 0;
-        // for (int i = 0; i < cityNum; i++) {
-        //     for (int j = 0; j < cityNum; j++) {
-        //         dis[i][j] = sc.nextInt();
-        //     }
-        //}
-
-        // int[][] dis = {{0, 2, 6, 5},
-        //     {2, 0, 4, 4},
-        //     {6, 4, 0, 2},
-        //     {5, 4, 2, 0}};
-            //boolean[] visited = new boolean[4];
-            //System.out.println(min_dis);
-            //backTrace(4, dis, 0, visited, 0);    
-            //System.out.println(min_dis);
-    //}
-            public static void main(String[] args) {
-                System.out.println("tsp");
+    static int min_dis = Integer.MAX_VALUE;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cityNum = sc.nextInt();
+        int[][] dis = new int[cityNum][cityNum];
+        boolean[] visited = new boolean[cityNum];
+        
+        for (int i = 0; i < cityNum; i++) {
+            for (int j = 0; j < cityNum; j++) {
+                dis[i][j] = sc.nextInt();  
             }
-    public static void backTrace(int n, int[][] dis, int index, boolean[] visited,
+            
+        }
+        visited[0] = true;
+        backTrace(cityNum, dis, 1, 0, visited, 0);   
+        System.out.println(min_dis);
+    }
+    public static void backTrace(int n, int[][] dis, int index,int curretn_city, boolean[] visited,
                          int current_length) {
         if(index == n) {
-            min_dis = current_length + dis[index-1][0];
+            int min = current_length + dis[curretn_city][0];
+            if(min < min_dis)
+                min_dis = min;
         }else {
             for (int i = 0; i < n; i++) {
-                if(!visited[i]&&(min_dis >= current_length+dis[inde][i])) {
+                if(!visited[i]&&(min_dis > current_length+dis[curretn_city][i])) {
                     visited[i]  = true;
-                    current_length += dis[inde][i];
-                    backTrace(n, dis, index+1, visited, current_length);
-                    current_length -= dis[inde][i];
+                    current_length += dis[curretn_city][i];
+                    backTrace(n, dis, index+1, i, visited, current_length);
+                    current_length -= dis[curretn_city][i];
                     visited[i] = false;
                 }
             }
-
         }
-    }
+    }    
 }
